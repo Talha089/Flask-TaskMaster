75fc369aba291db7c6be06ec29419982ef4d13b7
Softtik <softtiktech@gmail.com>
2021-07-20 03:53:59 +0500

    add a task get tasks and delete tasks

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, url_for, request, redirect
+# from flask_sqlalchemy import SQLALchemy
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# INITIALIZE DATABASE
+db = SQLAlchemy(app)
+
+#MODEL
+class TODO(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime,default=datetime.utcnow)
+
+    def __rep__(self):
+        return '<Task %r>' % self.id
+
+#ROUTE
+@app.route('/', methods=['POST','GET'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = TODO(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue while adding a new Task'    
+    else:
+        tasks = TODO.query.order_by(TODO.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = TODO.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was an error while deleting that Task'
+
+if __name__ == "__main__":
+    app.run(debug=True)
